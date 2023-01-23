3247bbf2dfafe3916f2fc5fbe4891061bc4e9a9c
SBars <samuelbarsuhn@gmail.com>
2023-01-23 15:28:35 -0600

    added content to the README and added comments to the code

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Weather-Dashboard
+
+## Description
+This website is a weather dashboard that will give you the current temperature and weather conditions along with a five day forcast so you can prepare for the week.
+It uses local storage to save your recent searches so you can easily switch between viewing the weather of different cities.
+During this project I furthered my knowledge about using local storage, I also learned how to use tamplate literals to create elements on a page.
+This was also my first time using an API in a website so I learned how to call from an API and use the information it passes back on a webpage.
+
+## Installation
+
+There is no intalation required for this prject. Just go to the website and type a city in the searchbar.
+
+## Usage
+
+Start using the application by searching for a city. It will be added to the recent search tab on the side of the page. The current information as well as the five day forecast will appear on the page. From there you can search for other cities. At any time click on one of the recent cities buttons to view the information from that city.
+
+![Application Preview](assets\images\weatherdashboardpreview.png)
+
+## Credits
+
+--Sam Barsuhn
+
+-- U of M Coding Bootcamp
+
+## License
+
+MIT license
--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,22 @@
 const searchEl = $(".searchbar");
-
+// This grabs the local storage when the page is loading. if there is nothing in the local storage it will set the array to be empty so there are no bugs
 let cityArray = JSON.parse(localStorage.getItem("cities")) || [];
+// This is here so all of the main functions will only execute after the page is loaded
 $(function () {
+  // This section uses Dayjs to grab the current date and displays it on the main card
   let currentdate = dayjs().format("ddd MMM D YYYY");
   $("#date").text(currentdate);
-
+  // This function creates the recent search buttons and lets the user click them to view other cities
   function setRecent(city) {
     let recentButton = $(`
       <button id="recentsearch">${city}</button>
       `);
-      $(recentcities).append(recentButton);
-      $(recentButton).on("click", function () {
-        getWeather($(this).text());
-        
-      });
-    }
+    $(recentcities).append(recentButton);
+    $(recentButton).on("click", function () {
+      getWeather($(this).text());
+    });
+  }
+  // This function takes the input from the search bar and puts it into the API call. It then fetches the information and adds it to the corresponding sections of the card
   function getWeather(city) {
     let apiUrl =
       "https://api.openweathermap.org/data/2.5/forecast?q=" +
@@ -55,7 +57,7 @@ $(function () {
 
         let lat = data.city.coord.lat;
         let lon = data.city.coord.lon;
-
+        // This section is for the five day forcast. It uses a different API call that uses latitude and longitude so it grabs that information from the first call and puts it into the new one
         fetch(
           "https://api.openweathermap.org/data/2.5/forecast?lat=" +
             lat +
@@ -63,13 +65,15 @@ $(function () {
             lon +
             "&units=imperial&appid=61aa3733fa83482d5be3ca86bd4a8fdf"
         )
+          // Since this fetch request has information from multiple times of the day on different days I decided tto have it just take the information from 12pm each day
           .then((response) => response.json())
           .then((weekresponse) => {
             let fiveDayArray = weekresponse.list.filter((day) =>
               day.dt_txt.includes("12:00:00")
             );
+            // This clears the previous cards if there were any so the page doesn't pile up with old cards
             $(".cardcontainer").empty();
-
+            // Then it takes that information and puts it onto 5 cards to display at the bottom of the page
             for (let i = 0; i < fiveDayArray.length; i++) {
               let today = new Date(fiveDayArray[i].dt_txt)
                 .toLocaleString()
@@ -101,27 +105,26 @@ $(function () {
           });
       });
   }
+  // This is what tells the geatWeather function what to use as the city input. It also puts the city input into local storage to be used later
   $(".searchbtn").on("click", function (event) {
     event.preventDefault();
     let city = $(".searchbar").val();
     cityArray.push(city);
     localStorage.setItem("cities", JSON.stringify(cityArray));
-   
+
     if (city) {
       setRecent(city);
       getWeather(city);
-      // want to clear the input field after a search
+      // Prevents the user from searching if there is nothing in the search bar
     } else {
       alert("please enter a city name");
     }
-
-   
   });
   for (let i = 0; i < cityArray.length; i++) {
     setRecent(cityArray[i]);
   }
+  // if the user has local storage information from the site already it will load the page while showing the information and forecast from the first item in the recent search array
   for (let i = 0; i < cityArray.length; i++) {
     getWeather(cityArray[0]);
   }
 });
-
