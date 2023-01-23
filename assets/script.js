f6fc65a372594296b0f1aed153e74d2497edce45
SBars <samuelbarsuhn@gmail.com>
2023-01-23 12:34:19 -0600

    fixed code with 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -1,82 +1,105 @@
-const temp = document.querySelectorAll(".temp");
 const searchEl = document.querySelector(".searchbar");
-const recentSearch = document.querySelector("#recentsearch");
+// NEED TO GET LOCAL STORASGE WORKING CORRECTLY
+
 $(function () {
   let currentdate = dayjs().format("ddd MMM D YYYY");
   $("#date").text(currentdate);
-  
+  localStorage.getItem(recentcities);
+
   $(".searchbtn").on("click", function (event) {
     event.preventDefault();
     let city = $(".searchbar").val();
     if (city) {
       searchEl.textContent = "";
-      localStorage.setItem(recentSearch, city);
-      console.log(localStorage);
-      $("#recentsearch").text(city);
+      setRecent();
       getWeather();
     } else {
       alert("please enter a city name");
     }
+    function setRecent() {
+      const recentObj = city;
+      // console.log(recentObj)
+      let recentButton = $(`
+        <button id="recentsearch">
+        ${recentObj}</button>
+        `);
+      $(recentcities).append(recentButton);
+      localStorage.setItem(recentButton, recentcities);
+    }
+    // NEED TO GET RECENT BUTTON TO WORK CORRECTLY
+    $("#recentsearch").on("click", function (event) {
+      event.preventDefault();
+      //  need to set city = to the text on the button
+
+      getWeather();
+    });
+
     function getWeather() {
       let apiUrl =
-      "https://api.openweathermap.org/data/2.5/forecast?q=" +
-      city +
-      "&units=imperial&appid=61aa3733fa83482d5be3ca86bd4a8fdf";
+        "https://api.openweathermap.org/data/2.5/forecast?q=" +
+        city +
+        "&units=imperial&appid=61aa3733fa83482d5be3ca86bd4a8fdf";
       fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {console.log(data)
-        let icon = data.list[0].weather[0].icon
-        let iconUrl = `https://openweathermap.org/img/w/${icon}.png`
-        
-        let cityName = $(`
+        .then((response) => response.json())
+        .then((data) => {
+          // console.log(data);
+          let icon = data.list[0].weather[0].icon;
+          let iconUrl = `https://openweathermap.org/img/w/${icon}.png`;
+
+          let cityName = $(`
         <div>${data.city.name}
         </div>
         
         `);
-            $("#name").append(cityName);
-        let temp = $(`
+          $("#name").html(cityName);
+
+          let temp = $(`
         <p> TEMP: ${data.list[0].main.temp}
-        </p>`)
-        $("#temp").append(temp)
-        
-        let wind = $(`
+        </p>`);
+          $("#temp").html(temp);
+
+          let wind = $(`
         <p> WIND: ${data.list[0].wind.speed}
-        </p>`)
-        $("#wind").append(wind)
-        
-        let humidity = $(`
+        </p>`);
+          $("#wind").html(wind);
+
+          let humidity = $(`
         <p> HUMIDITY: ${data.list[0].main.humidity}
-        </p>`)
-        $("#humidity").append(humidity)
-        
-        let iconEl = $(`
-        <img src = "${iconUrl}"
-        />`)
-        $(".big-one").append(iconEl)
-        
-        let lat = data.city.coord.lat
-        let lon = data.city.coord.lon
-        console.log(lat, lon)
-     
-        
-        
-        fetch(
-          "https://api.openweathermap.org/data/2.5/forecast?lat=" +
-          lat +
-          "&lon=" +
-          lon +
-          "&units=imperial&appid=61aa3733fa83482d5be3ca86bd4a8fdf"
+        </p>`);
+          $("#humidity").html(humidity);
+
+          let iconEl = $(`
+        <img src = ${iconUrl}
+        />`);
+          $(".image").html(iconEl);
+
+          let lat = data.city.coord.lat;
+          let lon = data.city.coord.lon;
+
+          fetch(
+            "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+              lat +
+              "&lon=" +
+              lon +
+              "&units=imperial&appid=61aa3733fa83482d5be3ca86bd4a8fdf"
           )
-          .then((response) => response.json())
-          .then((weekresponse) => {console.log(weekresponse)
-            
-            let fiveDayArray = weekresponse.list.filter(day => day.dt_txt.includes("12:00:00"))
-            for (let i = 0 ; i < fiveDayArray.length; i++) {
-              let today = new Date(fiveDayArray[i].dt_txt).toLocaleString().split(",")[0]
-              
-              let fiveDayIcon = fiveDayArray[i].weather[0].icon 
-              let fiveDayIconUrl = `https://openweathermap.org/img/w/${fiveDayIcon}.png`
-              let fiveDayCard = $(`
+            .then((response) => response.json())
+            .then((weekresponse) => {
+              // console.log(weekresponse);
+
+              let fiveDayArray = weekresponse.list.filter((day) =>
+                day.dt_txt.includes("12:00:00")
+              );
+              $(".cardcontainer").empty();
+
+              for (let i = 0; i < fiveDayArray.length; i++) {
+                let today = new Date(fiveDayArray[i].dt_txt)
+                  .toLocaleString()
+                  .split(",")[0];
+
+                let fiveDayIcon = fiveDayArray[i].weather[0].icon;
+                let fiveDayIconUrl = `https://openweathermap.org/img/w/${fiveDayIcon}.png`;
+                let fiveDayCard = $(`
               <div class="cards" id="cards">
               <h2>
               ${today}
@@ -94,15 +117,14 @@ $(function () {
               WIND: ${fiveDayArray[i].wind.speed}
               </p>
               </div>
-              `)
-              $(".cardcontainer").append(fiveDayCard);
-            }
-          })
+              `);
+                $(".cardcontainer").append(fiveDayCard);
+              }
+            });
         });
-      }
-    });
+    }
   });
-
+});
 
 // apiKey = "61aa3733fa83482d5be3ca86bd4a8fdf"
 
